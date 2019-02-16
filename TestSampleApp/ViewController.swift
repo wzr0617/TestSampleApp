a5cfd6a3c839bddc97b0e1477ab39f1bbea92abf
zhenru wang <wzr0617@gmail.com>
2019-02-17 01:40:57 +0900

    二行目場合、二行目場合インデント（インデントの値はタイトル通りです）

--- TestSampleApp/ViewController.swift
+++ TestSampleApp/ViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let titleName = "平仮名："
+        let font = UIFont(name: "HiraginoSans-W3", size: 12)
+        let size: CGSize? = titleName.size(withAttributes: [NSAttributedString.Key.font: font!])
+        
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.firstLineHeadIndent = 0
+        paragraph.headIndent = size?.width ?? 0 //You can change this to whatever indent you want
+        let mutString = NSAttributedString(
+            string: "平仮名：平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名平仮名",
+            attributes: [NSAttributedString.Key.paragraphStyle: paragraph]
+        )
+        
+        mainLabel.attributedText = mutString
         // Do any additional setup after loading the view, typically from a nib.
     }
 
